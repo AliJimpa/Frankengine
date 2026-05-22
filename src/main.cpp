5e8cc016ccbcc6de942416391dd32ead169f492d
Alijimpa <alibadpa007@gmail.com>
2026-05-22 10:50:48 +0330

    Implement BuildSetting And basic engine code

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+     int x = 5;
+     std::cout << "MainApp" << std::endl;
+     std::cin >> x;
+      std::cout << "MainApp" <<x<< std::endl;
+     std::cin.get();
+}
